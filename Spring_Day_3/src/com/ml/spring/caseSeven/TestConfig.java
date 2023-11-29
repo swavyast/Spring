24c2022d0a0ff86cd17206f46261e9ec4e2e8a51
Himanshu Rai <swavyast@gmail.com>
2023-11-29 09:44:59 +0530

    I0000000000000000000000000000000000000008

---
+++ Spring_Day_3/src/com/ml/spring/caseSeven/TestConfig.java
@@ -0,0 +1,21 @@
+package com.ml.spring.caseSeven;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ml.spring.caseThree.AppConfig;
+
+/**
+ * Issue remains the same even after manipulating autowiring.
+ * 
+ * Conclusion : no matter what, spring container is going to have only one bean per Type.
+ * */
+public class TestConfig {
+
+    public static void main(String[] args) {
+	ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+	System.out.println("\n\nSpring container is ready");
+	ctx.getBean("two");
+    }
+
+}
