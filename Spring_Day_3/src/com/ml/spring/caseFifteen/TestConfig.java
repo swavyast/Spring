5493a942e3c8b90eae2b25daad16b0610a0f3f61
Himanshu Rai <swavyast@gmail.com>
2023-12-01 17:14:46 +0530

    caseFifteen
    I0000000000000000000000000000000000000019

--- Spring_Day_3/src/com/ml/spring/caseFifteen/TestConfig.java
+++ Spring_Day_3/src/com/ml/spring/caseFifteen/TestConfig.java
@@ -1,10 +1,55 @@
 package com.ml.spring.caseFifteen;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Inside Runtime environment of JDK20
+ * 
+ * @Autowired(required = false)
+ * Output:
+ * 1.
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Default constructor in ClassOne
+ * Spring Container is ready
+ * 2.
+ * Default constructor in ClassOne
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Spring Container is ready
+ * 3.
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Default constructor in ClassOne
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Spring Container is ready
+ * 4.
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassOne
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Spring Container is ready
+ * 5.
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassOne
+ * Spring Container is ready
+ * */
 public class TestConfig {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+	
+	ApplicationContext ctx = new AnnotationConfigApplicationContext(com.ml.spring.caseNine.AppConfig.class);
+	System.out.println("Spring Container is ready");
     }
 
 }
