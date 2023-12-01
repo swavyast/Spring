a9546897b1806d0692f98f15834052966191f5b7
Himanshu Rai <swavyast@gmail.com>
2023-12-02 02:36:08 +0530

    I0000000000000000000000000000000000000009

---
+++ Spring_Day_4/src/com/ml/spring/caseNine/TestConfig.java
@@ -0,0 +1,58 @@
+package com.ml.spring.caseNine;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Inside Runtime environment of JDK20
+ * CaseOne :
+ * @Autowired(required = true)
+ * @Qualifier("someName")
+ * Bean("someName") of ClassTwo
+ * Bean("otherName") of ClassTwo without name.
+ * Output :
+ * 1.
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Default constructor in ClassOne
+ * Spring Container is ready
+ * 2.
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Default constructor in ClassOne
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Spring Container is ready
+ * 3.
+ * Default constructor in ClassOne
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Spring Container is ready
+ * 4.
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassOne
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Spring Container is ready
+ * 5.
+ * Default constructor in ClassTwo
+ * getClassTwo_Two() bean
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassOne
+ * Spring Container is ready
+ * */
+public class TestConfig {
+
+    public static void main(String[] args) {
+	ApplicationContext ctx = new AnnotationConfigApplicationContext(com.ml.spring.caseNine.AppConfig.class);
+	System.out.println("Spring Container is ready");
+	((ConfigurableApplicationContext)ctx).close();
+    }
+}
