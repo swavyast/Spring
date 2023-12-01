d61f231f6842055ebe97e78c02822e05eb78ba5e
Himanshu Rai <swavyast@gmail.com>
2023-12-02 01:44:46 +0530

    I0000000000000000000000000000000000000002

--- Spring_Day_4/src/com/ml/spring/caseTwo/TestConfig.java
+++ Spring_Day_4/src/com/ml/spring/caseTwo/TestConfig.java
@@ -18,11 +18,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * No qualifying bean of type 'com.ml.spring.ClassTwo' available: expected at least 1 bean which qualifies as autowire candidate.
  * Dependency annotations: {@org.springframework.beans.factory.annotation.Autowired(required=true),
  * @org.springframework.beans.factory.annotation.Qualifier("")}
+ * 2. 
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassOne
+ * Error creating bean with name 'getClassOne': Unsatisfied dependency expressed through field 'two';
+ * nested exception is org.springframework.beans.factory.NoSuchBeanDefinitionException:
+ * No qualifying bean of type 'com.ml.spring.ClassTwo' available: expected at least 1 bean which qualifies as autowire candidate.
+ * Dependency annotations: {@org.springframework.beans.factory.annotation.Autowired(required=true),
+ * @org.springframework.beans.factory.annotation.Qualifier("")}
  * */
 public class TestConfig {
 
     public static void main(String[] args) {
-	ApplicationContext ctx = new AnnotationConfigApplicationContext(com.ml.spring.caseOne.AppConfig.class);
+	ApplicationContext ctx = new AnnotationConfigApplicationContext(com.ml.spring.caseTwo.AppConfig.class);
 	System.out.println("Spring Container is ready");
 	((ConfigurableApplicationContext)ctx).close();
     }
