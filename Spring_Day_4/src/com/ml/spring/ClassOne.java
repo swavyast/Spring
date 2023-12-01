2f35cca75f7a5a71053545cc625562a26336dd8c
Himanshu Rai <swavyast@gmail.com>
2023-12-02 01:37:11 +0530

    I0000000000000000000000000000000000000001

---
+++ Spring_Day_4/src/com/ml/spring/ClassOne.java
@@ -0,0 +1,31 @@
+package com.ml.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class ClassOne {
+
+    private long id;
+    @Autowired(required = true)
+    @Qualifier
+    private ClassTwo two;
+
+    public ClassOne() {
+	System.out.println("Default constructor in ClassOne");
+    }
+
+    public long getId() {
+	return id;
+    }
+
+    public ClassTwo getTwo() {
+	System.out.println("Getter method in class one for ClassTwo dependency");
+	return two;
+    }
+
+    @Override
+    public String toString() {
+	return "ClassOne [id=" + id + "]";
+    }
+
+}
