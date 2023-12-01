22f904b50e67edc81a0eb0fe8a9c34205d6ee739
Himanshu Rai <swavyast@gmail.com>
2023-12-02 02:15:09 +0530

    I0000000000000000000000000000000000000007

---
+++ Spring_Day_4/src/com/ml/spring/caseSeven/TestConfig.java
@@ -0,0 +1,31 @@
+package com.ml.spring.caseSeven;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Inside Runtime environment of JDK20
+ * CaseOne :
+ * @Autowired(required = true)
+ * @Qualifier("someName")
+ * One Bean of ClassTwo without any name
+ * Output :
+ * 1.
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * Default constructor in ClassOne
+ * Spring Container is ready
+ * 2,
+ * Default constructor in ClassOne
+ * Default constructor in ClassTwo
+ * getClassTwo_One() bean
+ * */
+public class TestConfig {
+
+    public static void main(String[] args) {
+	ApplicationContext ctx = new AnnotationConfigApplicationContext(com.ml.spring.caseSeven.AppConfig.class);
+	System.out.println("Spring Container is ready");
+	((ConfigurableApplicationContext)ctx).close();
+    }
+}
